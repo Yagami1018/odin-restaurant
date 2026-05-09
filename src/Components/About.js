47277dd151c5223518e4de7b7d685bc44eaa5fb6
XNAK3 <yagami01018@gmail.com>
2026-05-09 16:00:21 -0400

    Implement About component as an unordered list

--- src/Components/About.js
+++ src/Components/About.js
@@ -2,7 +2,25 @@ import { content } from "./Content.js";
 
 export default function About() {
   content.innerHTML = "";
+
+  //DOM References
   const newElement = document.createElement("h1");
+  const ul = document.createElement("ul");
+  const address = document.createElement("li");
+  const email = document.createElement("li");
+  const phone = document.createElement("li");
+
+  //Text Contents
   newElement.textContent = `Welcome to ${this.textContent}`;
+  address.textContent =
+    "Fake Adress: 1st between 20th avenue and Malcolm Street, Harlem Neighborhood, California";
+  email.textContent = "Email: fakeaccount@fakemail.com";
+  phone.textContent = "Phone: 1-1456-7854";
+
+  ul.appendChild(address);
+  ul.appendChild(email);
+  ul.appendChild(phone);
+
   content.appendChild(newElement);
+  content.appendChild(ul);
 }
