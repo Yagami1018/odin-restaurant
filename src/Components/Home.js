f688573c38f15fb9f2fc78ab0af1421cc070e431
XNAK3 <yagami01018@gmail.com>
2026-05-11 14:48:45 -0400

    Implement Menu with 4 custom dishes, tweak some bugs, fixed layout, set responsive design

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -6,11 +6,34 @@ export default function Home() {
   //DOM References
   const newElement = document.createElement("h1");
   const welcome = document.createElement("p");
-
+  document.body.style.minWidth="100vw"
   //Text Contents
   newElement.textContent = `Welcome to ${this.textContent}`;
-  welcome.textContent = `Welcome to my Restaurant. We are proud to receive you, we know for sure that you likely fall in love with our tasteful meals, prepared with love an effort for clients like you that decided to visit one of the most excellent restaurants of our beloved city. Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat ipsam perspiciatis consequatur deserunt repellendus. Nulla in iste rerum quibusdam ipsum at cumque, eaque atque voluptate repudiandae sunt, voluptas, soluta vitae. Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat ipsam perspiciatis consequatur deserunt repellendus. Nulla in iste rerum quibusdam ipsum at cumque, eaque atque voluptate repudiandae sunt, voluptas, soluta vitae. Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat ipsam perspiciatis consequatur deserunt repellendus. Nulla in iste rerum quibusdam ipsum at cumque, eaque atque voluptate repudiandae sunt, voluptas, soluta vitae. Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat ipsam perspiciatis consequatur deserunt repellendus. Nulla in iste rerum quibusdam ipsum at cumque, eaque atque voluptate repudiandae sunt, voluptas, soluta vitae. Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat ipsam perspiciatis consequatur deserunt repellendus. Nulla in iste rerum quibusdam ipsum at cumque, eaque atque voluptate repudiandae sunt, voluptas, soluta vitae. Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat ipsam perspiciatis consequatur deserunt repellendus. Nulla in iste rerum quibusdam ipsum at cumque, eaque atque voluptate repudiandae sunt, voluptas, soluta vitae. Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat ipsam perspiciatis consequatur deserunt repellendus. Nulla in iste rerum quibusdam ipsum at cumque, eaque atque voluptate repudiandae sunt, voluptas, soluta vitae.`;
+  welcome.textContent = `Welcome to my Restaurant. We are proud to receive you,
+                        we know for sure that you likely fall in love with our 
+                        tasteful meals, prepared with love an effort for clients like you 
+                        that decided to visit one of the most excellent restaurants of our
+                        beloved city. Lorem ipsum dolor sit amet consectetur adipisicing 
+                        elit. Repellat ipsam perspiciatis consequatur deserunt repellendus.
+                        Nulla in iste rerum quibusdam ipsum at cumque, eaque atque voluptate
+                        repudiandae sunt, voluptas, soluta vitae. Lorem ipsum dolor sit amet
+                        consectetur adipisicing elit. Repellat ipsam perspiciatis consequatur
+                        deserunt repellendus. Nulla in iste rerum quibusdam ipsum at cumque, 
+                        eaque atque voluptate repudiandae sunt, voluptas, soluta vitae. 
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat
+                        ipsam perspiciatis consequatur deserunt repellendus. Nulla in iste 
+                        rerum quibusdam ipsum at cumque, eaque atque voluptate repudiandae sunt,
+                        voluptas, soluta vitae. Lorem ipsum dolor sit amet consectetur adipisicing
+                        elit. Repellat ipsam perspiciatis consequatur deserunt repellendus. Nulla 
+                        in iste rerum quibusdam ipsum at cumque, eaque atque voluptate repudiandae 
+                        sunt, voluptas, soluta vitae. Lorem ipsum dolor sit amet consectetur adipisicing
+                        elit. Repellat ipsam perspiciatis consequatur deserunt repellendus. Nulla in iste rerum 
+                        quibusdam ipsum at cumque, eaque atque voluptate repudiandae sunt, voluptas, soluta vitae.
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat ipsam perspiciatis consequatur
+                        deserunt repellendus. Nulla in iste rerum quibusdam ipsum at cumque, eaque atque voluptate repudiandae
+                        sunt, voluptas, soluta vitae. Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat ipsam
+                        perspiciatis consequatur deserunt repellendus. Nulla in iste rerum quibusdam ipsum at cumque, eaque
+                        atque voluptate repudiandae sunt, voluptas, soluta vitae.`;
 
-  content.appendChild(newElement);
-  content.appendChild(welcome);
+  content.append(newElement,welcome);
 }
